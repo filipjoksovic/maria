f7252fd42d8080f7cbcd6b4e0cdf83f8b52cc1f2
Filip <filipjoksovic1@gmail.com>
2024-11-10 23:33:30 +0100

    MARIA-14 Load up request body on request page load

--- src/components/request-body/request-body.component.ts
+++ src/components/request-body/request-body.component.ts
@@ -1,6 +1,6 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {CodeEditorComponent, CodeModel} from "@ngstack/code-editor";
-import {BehaviorSubject, debounceTime} from "rxjs";
+import {BehaviorSubject, debounceTime, skip} from "rxjs";
 
 @Component({
   selector: 'app-request-body',
@@ -9,7 +9,7 @@ import {BehaviorSubject, debounceTime} from "rxjs";
   templateUrl: './request-body.component.html',
   styleUrl: './request-body.component.scss'
 })
-export class RequestBodyComponent {
+export class RequestBodyComponent implements OnInit {
 
   private _requestContentChanged$: BehaviorSubject<string> = new BehaviorSubject('');
   private requestContentChanged$ = this._requestContentChanged$.asObservable();
@@ -17,28 +17,43 @@ export class RequestBodyComponent {
   @Output()
   public requestBodyChanged: EventEmitter<string> = new EventEmitter<string>();
 
-  theme = 'vs-dark';
+  @Input()
+  public body: string = "{}";
 
-  model: CodeModel = {
-    language: 'json',
-    uri: 'main.json',
-    value: '{}'
-  };
+  theme = 'vs-light';
+
+  model!: CodeModel;
 
   options = {
     contextmenu: true,
     minimap: {
-      enabled: true
+      enabled: false
     }
   };
 
   constructor() {
-    this.requestContentChanged$.pipe(debounceTime(1000)).subscribe((value) => {
+    this.requestContentChanged$.pipe(skip(1), debounceTime(1000)).subscribe((value) => { //TODO figure out a way to remove skip()
       console.log('Request body changed:', value);
       this.requestBodyChanged.emit(value);
     });
   }
 
+  public ngOnInit() {
+    let parsedBody = "";
+
+    try {
+      parsedBody = JSON.stringify(JSON.parse(this.body), null, "\t");
+    } catch (e) {
+      parsedBody = this.body;
+      console.error('Failed to parse JSON:', e);
+    }
+
+    this.model = {
+      language: 'json',
+      uri: 'main.json',
+      value: parsedBody
+    };
+  }
 
   onCodeChanged(value: string) {
     this._requestContentChanged$.next(value);
