ba97d1bca16734166650acd0678ef4ca7a6a7d87
Filip <filipjoksovic1@gmail.com>
2024-11-11 18:20:45 +0100

    MARIA-19 Migrate to monaco editor to display request result

--- src/components/request-result/request-result.component.ts
+++ src/components/request-result/request-result.component.ts
@@ -1,9 +1,10 @@
-import {Component, inject, Signal} from '@angular/core';
+import {Component, effect, inject, model, Signal} from '@angular/core';
 import {RequestResultService} from "../../app/services/request-result.service";
 import {toSignal} from "@angular/core/rxjs-interop";
 import {MatFormField, MatFormFieldModule} from "@angular/material/form-field";
 import {MatInput, MatInputModule} from "@angular/material/input";
 import {FormsModule} from "@angular/forms";
+import {CodeEditorComponent, CodeModel} from "@ngstack/code-editor";
 
 @Component({
   selector: 'app-request-result',
@@ -11,13 +12,34 @@ import {FormsModule} from "@angular/forms";
   imports: [
     FormsModule,
     MatInputModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    CodeEditorComponent
   ],
   templateUrl: './request-result.component.html',
   styleUrl: './request-result.component.scss'
 })
 export class RequestResultComponent {
+  public model: CodeModel = {
+    language: 'json',
+    uri: 'main.json',
+    value: "{}"
+  };
+
+  public options = {
+    contextmenu: true,
+    minimap: {
+      enabled: false
+    },
+    readOnly: true
+  };
+
   private readonly requestResultService = inject(RequestResultService);
 
   public requestResult: Signal<string | undefined> = toSignal(this.requestResultService.requestResult$);
+
+  constructor() {
+    effect(() => {
+      this.model.value = this.requestResult() ?? "{}";
+    });
+  }
 }
